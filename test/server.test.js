27d062ad09a20cfb6f6b86645b17c88e19036cad
shmck <shawn.j.mckay@gmail.com>
2020-03-17 20:57:42 -0700

    6.6 uppercase when MESSAGE_STYLE is uppercase

--- test/server.test.js
+++ test/server.test.js
@@ -6,6 +6,8 @@ const util = require("util");
 // node 12
 const { readFile } = require("fs");
 const path = require("path");
+const sinon = require("sinon");
+let sandbox = sinon.createSandbox();
 
 const readFileAsync = util.promisify(readFile);
 
@@ -30,8 +32,23 @@ describe("server", () => {
         assert.ok(response.text.match(/body {/));
       });
   });
-  // 5.1
+  // 6.5
+  it("should load the dotenv config", async () => {
+    const serverFilePath = path.join(process.cwd(), "src", "server.js");
+    const serverFile = await readFile(serverFilePath, "utf8");
+    const lines = serverFile.split("/n");
+    const firstLines = lines.slice(0, 5);
+    let match = false;
+    for (const line of firstLines) {
+      if (line.match(/require.+dotenv.+\.config()/)) {
+        match = true;
+      }
+    }
+    assert.ok(match, "should load dotenv config at the top of the file");
+  });
+  // 6.6
   it('should return json from the "/json" endpoint', () => {
+    sandbox.stub(process.env, "MESSAGE_STYLE").value("");
     return request(server)
       .get("/json")
       .expect("Content-type", /application\/json/)
@@ -44,21 +61,23 @@ describe("server", () => {
           'Message should be "Hello json"'
         );
       });
+    sandbox.restore();
   });
-  // 6.5
-  it("should load the dotenv config", async () => {
-    const serverFilePath = path.join(process.cwd(), "src", "server.js");
-    const serverFile = await readFileAsync(serverFilePath, "utf8");
-    const lines = serverFile.split("/n");
-    const firstLines = lines.slice(0, 5);
-    let match = false;
-    for (const line of firstLines) {
-      if (line.match(/require.+dotenv.+\.config()/)) {
-        match = true;
-      }
-    }
-    console.log(match);
-    assert.ok(match, "should load dotenv config at the top of the file");
+  it('should return json from the "/json" endpoint', () => {
+    sandbox.stub(process.env, "MESSAGE_STYLE").value("uppercase");
+    return request(server)
+      .get("/json")
+      .expect("Content-type", /application\/json/)
+      .expect(200)
+      .then(response => {
+        assert.ok(response.body, "Body does not contain json");
+        assert.equal(
+          response.body.message,
+          "HELLO JSON",
+          'Message should be "HELLO JSON" when process.env.MESSAGE_STYLE is "uppercase"'
+        );
+      });
+    sandbox.restore();
   });
   after(() => {
     server.close();
