4a4aa484417c0c2ac76639e8d9bdd170f05f40c1
vproskurin <vladimir.proskurin@gmail.com>
2020-05-14 21:41:58 -0700

    Save progress

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
-const express = require("express");
-
+const express = require("express", "path");
+const path = require("path");
 const app = express();
 
 console.log("Hello World");
 
 const server = app.listen(process.env.PORT || 3000);
 
-app.get("/", handler);
+app.get("/", fileHandler);
 
 function handler(req, res) {
   res.send("Hello Express");
+}
+
+function fileHandler(req, res) {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
 }
 
 // -- DO NOT EDIT BELOW THIS LINE
